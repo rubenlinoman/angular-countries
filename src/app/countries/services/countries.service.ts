5cd64120022741a1e359ff69e87670fadb78d17e
Rubén Lino <rubenlinomandado@gmail.com>
2023-09-05 18:09:20 +0200

    Sección 9. Vídeo 123

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
+
+
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) { }
+
+  searchCapital(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([])) // Devolver un arreglo vacío para seguir con el programa en caso de error
+      );
+  }
+
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([])) // Devolver un arreglo vacío para seguir con el programa en caso de error
+      );
+  }
+
+  searchRegion(region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`;
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([])) // Devolver un arreglo vacío para seguir con el programa en caso de error
+      );
+  }
+  
+}
